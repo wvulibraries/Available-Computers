dc5ec852b19e260373a5d031750aee04602f7313
msblake <msblake@ca2c5cd7-39d7-495c-9a21-ea95cd046ca5>
2010-11-15 16:00:18 +0000

    Tagged availableComputers version 1.0 and moved all files from trunk to tag.

--- headerIncludes.php
+++
@@ -1,5 +0,0 @@
-<link rel="stylesheet" href="/availableComputers/includes/css/main.css" type="text/css" media="screen" />
-<link rel="stylesheet" href="/availableComputers/includes/css/tableLocations.css" type="text/css" media="screen" />
-
-<script type="text/javascript" src="/availableComputers/includes/js/main.js"></script>
-<script type="text/javascript" src="/availableComputers/includes/js/validate.js"></script>
