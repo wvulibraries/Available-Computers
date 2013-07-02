6fd4cfe766377b80a606750253540fb64a0417f6
Scott Blake <Scott.Blake@mail.wvu.edu>
2013-07-02 12:50:59 -0400

    Update header

---
+++ engineInit.php
@@ -0,0 +1,6 @@
+<?php
+require_once '/home/systems.lib.wvu.edu/phpincludes/engine/engineAPI/3.2/engine.php';
+$engine = EngineAPI::singleton();
+
+errorHandle::errorReporting(errorHandle::E_ALL);
+?>
--- header.php
+++ header.php
@@ -1,12 +1,15 @@
-<?
-$engineDir = "/home/library/phpincludes/engineAPI/engine";
-include($engineDir ."/engine.php");
-$engine = new EngineCMS();
+<?php
+require_once 'engineInit.php';
 
-$engine->localVars('pageTitle',"Available Computers");
+recurseInsert("includes/engineHeader.php","php");
+$engine->eTemplate("load","library2012.2col");
 
 recurseInsert("dbTableList.php","php");
+require_once '/home/library/phpincludes/databaseConnectors/database.lib.wvu.edu.remote.php';
 $engine->dbConnect("database","availableComputers",TRUE);
+
+
+localVars::add('pageTitle',"Available Computers");
 
 $engine->eTemplate("load","systems");
 $engine->eTemplate("include","header");
