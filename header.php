08b118b6488ac242b0818e9f4518f12f46330a3c
Scott Blake <Scott.Blake@mail.wvu.edu>
2013-07-08 10:35:09 -0400

    Don't call template header within header

--- header.php
+++ header.php
@@ -1,15 +1,8 @@
 <?php
 require_once 'engineInit.php';
 
-// recurseInsert("includes/engineHeader.php","php");
-// $engine->eTemplate("load","library2012.2col");
-
 require_once '/home/library/phpincludes/databaseConnectors/database.lib.wvu.edu.remote.php';
 $engine->dbConnect("database","availableComputers",TRUE);
 
-
 localVars::add('pageTitle',"Available Computers");
-
-$engine->eTemplate("load","systems.2013.2col");
-$engine->eTemplate("include","header");
 ?>
