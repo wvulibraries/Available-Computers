edbbde3cab3ba39458719f3282c0a177000f3795
Michael Bond <mbond@th-forgotten.org>
2013-08-07 14:07:04 -0400

    engine header include

--- engineInit.php
+++ engineInit.php
@@ -2,5 +2,7 @@
 require_once '/home/library/phpincludes/engine/engineAPI/3.2/engine.php';
 $engine = EngineAPI::singleton();
 
+require_once "/home/library/public_html/includes/engineHeader.php";
+
 errorHandle::errorReporting(errorHandle::E_ALL);
 ?>
