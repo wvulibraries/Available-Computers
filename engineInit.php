a736dd9aa1ca2e7a6ef8d2b9cef1682d3ad767c1
Michael Bond <mbond@th-forgotten.org>
2013-08-07 13:00:38 -0400

    changed the directory to include engine from

--- engineInit.php
+++ engineInit.php
@@ -1,5 +1,5 @@
 <?php
-require_once '/home/systems.lib.wvu.edu/phpincludes/engine/engineAPI/3.2/engine.php';
+require_once '/home/library/phpincludes/engine/engineAPI/3.2/engine.php';
 $engine = EngineAPI::singleton();
 
 errorHandle::errorReporting(errorHandle::E_ALL);
