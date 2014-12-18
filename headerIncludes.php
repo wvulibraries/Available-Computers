bc98e7abf77b0102d9b0f1cc4016d2d5f0cacb17
David Gersting <dgersting@gmail.com>
2014-12-17 23:39:44 -0500

    Updated CSS to work on new website

--- headerIncludes.php
+++ headerIncludes.php
@@ -1,6 +1,8 @@
-<link rel="stylesheet" href="/availableComputers/includes/css/main.css" type="text/css" media="screen" />
-<link rel="stylesheet" href="/availableComputers/includes/css/tableLocations.css" type="text/css" media="screen" />
+<!-- <link rel="stylesheet" href="/services/computers/availableComputers/includes/css/main.css" type="text/css" media="screen" /> -->
+<link rel="stylesheet" href="/services/computers/availableComputers/includes/css/main.css" type="text/css" media="screen" />
+<link rel="stylesheet" href="/services/computers/availableComputers/includes/css/tableLocations.css" type="text/css" media="screen" />
+<link rel="stylesheet" href="/css/2014/webapps.css" type="text/css" media="screen" />
 
-<script type="text/javascript" src="/availableComputers/includes/js/main.js"></script>
+<script type="text/javascript" src="/services/computers/availableComputers/includes/js/main.js"></script>
 <script type="text/javascript" src="{engine var="engineListObjJS"}"></script>
 <script type="text/javascript" src="{engine var="selectBoxJS"}"></script>
